1be451a2b3c999afe79cf3f5eb9908c8ca2a95b4
Regy Cekerri <s1082145@studenti.univpm.it>
2021-03-04 00:00:13 +0100

    Aggiunto un commento javadoc

--- OPENWEATHERAPP/src/main/java/com/progettoPO/OPENWEATHERAPP/OpenweatherappApplication.java
+++ OPENWEATHERAPP/src/main/java/com/progettoPO/OPENWEATHERAPP/OpenweatherappApplication.java
@@ -3,6 +3,14 @@ package com.progettoPO.OPENWEATHERAPP;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * <b>Classe</b> che permette l'avvio dell'applicazione Spring Boot
+ * </p>
+ * 
+ * @author Cekerri Regy
+ * @author Vigliotta Michele
+ */
 @SpringBootApplication
 public class OpenweatherappApplication {
 
